fa97456753ca63573f9f0db04616a06a3325edeb
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-02 13:17:40 -0300

    feat: implementando model de categorias

--- todolist/category/models.py
+++ todolist/category/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    category_name = models.CharField(
+        max_length=128
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
