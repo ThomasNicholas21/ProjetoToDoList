b1047a5af953bc0f0936468415f42cd101a5f36a
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-02 13:17:59 -0300

    feat: implementando atividade em admin

--- todolist/activity/admin.py
+++ todolist/activity/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Activity
 
 # Register your models here.
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'priority'
+    list_display_links = 'id',
