d35eaf873565eb04edf88f559f0740e2e6802c88
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-02 13:18:12 -0300

    feat: implementando categoria em admin

--- todolist/category/admin.py
+++ todolist/category/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category
 
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'id', 'category_name',
+    list_display_links = 'id',
