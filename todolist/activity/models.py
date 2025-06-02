2659c2f4556693b066c513fd11a9193e717415b0
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-02 13:17:26 -0300

    feat: criando models de atividades

--- todolist/activity/models.py
+++ todolist/activity/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Activity(models.Model):
+    class Meta:
+        verbose_name = 'Activity'
+        verbose_name_plural = 'Activities'
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='activity'
+    )
+    title = models.CharField(
+        max_length=128
+    )
+    description = models.TextField()
+    due_date = models.DateTimeField()
+    status = models.CharField(
+        max_length=15,
+        choices=(
+            ('in_progress', 'In progress'),
+            ('late', 'Late'),
+            ('finished', 'Finished'),
+        )
+    )
+    priority = models.CharField(
+        max_length=15,
+        choices=(
+            ('low', 'Low'),
+            ('medium', 'Medium'),
+            ('high', 'High'),
+        )
+    )
+    finished_at = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
