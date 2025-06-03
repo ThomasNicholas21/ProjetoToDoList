64173a5b21ea3b0541059eba33d8f074ac056452
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 10:26:20 -0300

    test: faze RED para url detail de activity

--- todolist/activity/tests/test_urls.py
+++ todolist/activity/tests/test_urls.py
@@ -5,5 +5,9 @@ from django.urls import reverse
 
 class TestUrlActivity(TestCase):
     def test_url_activity_is_correct(self):
-        activity_get_url = reverse('activity:activity-api')
-        self.assertEqual(activity_get_url, '/api/activity/')
+        activity_url = reverse('activity:activity-api')
+        self.assertEqual(activity_url, '/api/activity/')
+
+    def test_url_activity_detail_is_correct(self):
+        activity_detail_url = reverse('activity:activity-detail-api')
+        self.assertEqual(activity_detail_url, kwargs={'activity_id': 1})
