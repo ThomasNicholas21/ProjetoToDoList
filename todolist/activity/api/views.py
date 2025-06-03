30815c6e29e180a534aae642eeeb6927da3b976e
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 12:27:20 -0300

    feat: fase Green, implementando get para activity-api

--- todolist/activity/api/views.py
+++ todolist/activity/api/views.py
@@ -4,7 +4,8 @@ from rest_framework.response import Response
 # Create your views here.
 
 class ActivityApiView(APIView):
-    ...
+    def get(self, *args, **kwargs):
+        return Response('GET ACTIVITY')
     
 
 class ActivityApiDetailView(APIView):
