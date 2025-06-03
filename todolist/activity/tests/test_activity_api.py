e68b661dfc73183d95f2584f1352a52652381e29
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 12:25:53 -0300

    test: fase RED para verificar retorno do endpoint activity-api

---
+++ todolist/activity/tests/test_activity_api.py
@@ -0,0 +1,13 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from activity.tests.test_activity_base import ActivityMixin
+
+
+class TestApiActivity(APITestCase, ActivityMixin):
+    def test_activity_api_get_returns_status_code_200(self):
+        activity_url = reverse('activity:activity-api')
+        response = self.client.get(activity_url)
+        self.assertEqual(
+            response.status_code,
+            200
+        )
