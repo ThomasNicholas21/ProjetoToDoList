0e0d79655dd38a8df4af75681742848255e9f36d
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 12:30:01 -0300

    test: faze RED para post do endpoint activity-api

--- todolist/activity/tests/test_activity_api.py
+++ todolist/activity/tests/test_activity_api.py
@@ -3,10 +3,18 @@ from django.urls import reverse
 from activity.tests.test_activity_base import ActivityMixin
 
 
-class TestApiActivity(APITestCase, ActivityMixin):
+class TestApiActivityStatusCode(APITestCase, ActivityMixin):
     def test_activity_api_get_returns_status_code_200(self):
         activity_url = reverse('activity:activity-api')
         response = self.client.get(activity_url)
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+
+    def test_activity_api_post_returns_status_code_200(self):
+        activity_url = reverse('activity:activity-api')
+        response = self.client.post(activity_url)
         self.assertEqual(
             response.status_code,
             200
