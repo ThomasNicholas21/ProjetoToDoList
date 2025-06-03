a7ddc7adb2ad29720ef9130261bb2070986e633c
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 20:11:22 -0300

    test: fase red, teste de status para activity atrasada

--- todolist/activity/tests/test_activity_api_data.py
+++ todolist/activity/tests/test_activity_api_data.py
@@ -2,10 +2,13 @@ from rest_framework.test import APITestCase
 from activity.api.serializer import ActivitySerializers
 from activity.models import Activity
 from django.urls import reverse
+from django.utils import timezone
+from datetime import datetime
 from activity.tests.test_activity_base import ActivityMixin
 
 
 class TestApiActivityData(APITestCase, ActivityMixin):
+    # endpoints GET
     def test_activity_api_get_returns_valid_data(self):
         data = self.make_activity_in_batch(amount=5)
         activity_url = reverse('activity:activity-api')
@@ -13,6 +16,7 @@ class TestApiActivityData(APITestCase, ActivityMixin):
         serializer = ActivitySerializers(data, many=True)
         self.assertEqual(response.data, serializer.data)
 
+    # endpoints POST
     def test_activity_api_post_returns_valid_data(self):
         payload = self.make_activity_payload()
         url = reverse('activity:activity-api')
@@ -21,6 +25,15 @@ class TestApiActivityData(APITestCase, ActivityMixin):
         serializer = ActivitySerializers(Activity.objects.get(id=response.data["id"]))
 
         self.assertEqual(response.data, serializer.data)
+    
+    def test_activity_api_post_returns_valid_data_status_late(self):
+        payload = self.make_activity_payload()
+        payload['due_date'] = timezone.make_aware(datetime(year=2024, month=12, day=30))
+
+        url = reverse('activity:activity-api')
+        response = self.client.post(url, data=payload, format='json')
+
+        self.assertEqual(response.data['status'], 'late')
 
 
 class TestApiDetailActivityStatusCode(APITestCase, ActivityMixin):
