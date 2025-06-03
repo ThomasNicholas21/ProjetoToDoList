488a78542f27682446a91886f3ece3df6d615c2e
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 10:36:25 -0300

    feat: criando url, faze green, detail activity

--- todolist/activity/api/urls.py
+++ todolist/activity/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from activity.api.views import ActivityApiView
+from activity.api.views import ActivityApiView, ActivityApiDetailView
 
 
 app_name='activity'
 
 
 urlpatterns = [
-    path('api/activity/', ActivityApiView.as_view(), name='activity-api')
+    path('api/activity/', ActivityApiView.as_view(), name='activity-api'),
+    path('api/activity/<int:activity_id>/', ActivityApiDetailView.as_view(), name='activity-detail-api')
 ]
