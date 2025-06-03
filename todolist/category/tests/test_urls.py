3347d436e72c62a189c2f6d3d6e6ebe84c91dae7
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 11:25:45 -0300

    test: ajuste faze red para detail category

--- todolist/category/tests/test_urls.py
+++ todolist/category/tests/test_urls.py
@@ -7,3 +7,7 @@ class TestUrlCategory(TestCase):
     def test_url_category_is_correct(self):
         category_url = reverse('category:category-api')
         self.assertEqual(category_url, '/api/category/')
+
+    def test_url_category_detail_is_correct(self):
+        category_detail_url = reverse('category:category-datail-api', kwargs={'category_id': 1})
+        self.assertEqual(category_detail_url, '/api/category/<int:category_id>')
