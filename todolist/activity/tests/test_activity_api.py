c7328d4b174fc0493782ae4ed3a828f685eb70cf
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-03 13:22:28 -0300

    test: fase red para delete de detail activity

--- todolist/activity/tests/test_activity_api.py
+++ todolist/activity/tests/test_activity_api.py
@@ -63,3 +63,17 @@ class TestApiDetailActivityStatusCode(APITestCase, ActivityMixin):
             response.status_code,
             200
         )
+
+    def test_activity_api_detail_delete_returns_status_code_200(self):
+        activity = self.make_activity()
+        activity_detail_url = reverse(
+            'activity:activity-detail-api', 
+            kwargs={
+                'activity_id': activity.pk
+                }
+            )
+        response = self.client.delete(activity_detail_url)
+        self.assertEqual(
+            response.status_code,
+            200
+        )
