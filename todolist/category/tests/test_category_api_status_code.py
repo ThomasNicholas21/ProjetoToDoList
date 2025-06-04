849ab4cd5b7a028509647686157374a91b48d961
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-06-04 14:59:59 -0300

    test: fase red, fazendo tratamento de erro para bad request e 404

--- todolist/category/tests/test_category_api_status_code.py
+++ todolist/category/tests/test_category_api_status_code.py
@@ -26,8 +26,8 @@ class TestApiCategoryStatusCode(APITestCase, CategoryMixin):
     def test_category_api_post_returns_status_code_201(self):
         valid_category = self.make_category_payload()
 
-        activity_url = reverse('category:category-api')
-        response = self.client.post(activity_url, data=valid_category, format='json')
+        category_url = reverse('category:category-api')
+        response = self.client.post(category_url, data=valid_category, format='json')
 
         self.assertEqual(
             response.status_code,
@@ -59,3 +59,29 @@ class TestApiDetailCategoryStatusCode(APITestCase, CategoryMixin):
             response.status_code,
             200
         )
+
+    def test_category_api_detail_get_returns_status_code_404(self):
+        category_detail_url = reverse(
+            'category:category-detail-api', 
+            kwargs={
+                'category_id': 9999
+                }
+            )
+        response = self.client.get(category_detail_url)
+        self.assertEqual(
+            response.status_code,
+            404
+        )
+
+    @patch('category.api.views.Category.objects.get') 
+    def test_get_category_api_detail_get_returns_status_code_500(self, mock_get):
+        mock_get.side_effect = Exception("Erro simulado")
+        category = self.make_category()
+
+        url = reverse(
+            'category:category-detail-api',
+            kwargs={'category_id': category.pk}
+        )
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 500)
